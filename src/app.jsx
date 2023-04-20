985b8696b748e9b6d73a601655bdc13b61bee906
GloLeah <127737786+GloLeah@users.noreply.github.com>
2023-04-20 23:51:01 +0300

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Notes from "./components/notes";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import App from "./app";
+const react = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
